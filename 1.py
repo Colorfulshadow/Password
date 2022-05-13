e45a8783ced3a9cc1d5e0dfcd039a9b3f036a9b4
Colorfulshadow <colorfulshadowty@gmail.com>
2022-05-13 14:38:44 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import hashlib
+str = ''
+
+md5_obj=hashlib.md5("".encode("utf-8"))
+md5_obj.update(str.encode("utf-8"))
+
+print('MD5加密前为 ：' + str)
+print('MD5加密后为 ：' + md5_obj.hexdigest())
+print('MD5加密后前十二位是：'+ md5_obj.hexdigest()[0:12])
+print('通过此种方法生成的密码是：Zty'+md5_obj.hexdigest()[0:12]+'!@#')
